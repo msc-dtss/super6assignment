ef01adce6e474498dd51b1094f64ab65a12ebd10
Dosaki <lord.g.bug@gmail.com>
2019-10-11 20:53:51 +0100

    Added games into the home page driven by the database.

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   gameService.fetchFuture(req.app).then((games) => {
-    console.log(games)
-    res.render('index', { title: 'Super6 Rugby', games: games});
+    res.render('index', { title: 'Super6 Rugby', games: games });
   }, 
   (reason) => {
     console.log(reason)
