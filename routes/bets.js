977ffe81712c112458c684429bda6248c7d58d72
Christopher Smith <chris171819@googlemail.com>
2019-10-25 21:16:47 +0100

    edit of bets.js to accept user bet object

--- routes/bets.js
+++ routes/bets.js
@@ -15,17 +15,19 @@ router.get('/bets/games', function(req, res, next) {
   });
 });
 
-router.post('/bets', function (req, res, next) {    
-
+router.post('/bets', function (req, res, next) {
     const bet = { 
-        roundId: req.body.roundId, 
-        gameId: req.body.gameId, 
-        winTeam: req.body.winTeam, 
-        teamATries: req.body.teamATries, 
-        teamBTries: req.body.teamBTries, 
-        userId: req.body.userId, 
-        goldenTry: req.body.goldenTry 
+        roundId: Number(req.body.roundId), 
+        userId: Number(req.body.userId), 
+        games: []
     };
+    req.body.games.forEach((game) => {
+      bet.games.push({
+        teamATries: Math.max(Number(game.teamATries), 0),
+        teamBTries: game.teamBTries,
+        //...
+      });
+    });
 
     const db = req.app.get('super6db');
 
