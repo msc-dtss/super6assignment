116d265f47c54305d20d5f43ad584afceadb17f0
Christopher Smith <chris171819@googlemail.com>
2019-10-26 21:36:35 +0100

    Now collects the users golden try input

--- public/javascript/play.js
+++ public/javascript/play.js
@@ -4,20 +4,24 @@ var placeBet = () => {
 }
 
 var getBetValues = () => {
-    var bet = {
-        "userId": "",
-        "roundId": 1,
-        "games": []
-    };
-    for (i = 0; i < 6; i++ ){
-        let teamATries = document.querySelector(`[game_block_${i}] input[team_A_tries]`)
-        let teamBTries = document.querySelector(`[game_block_${i}] input[team_B_tries]`)
-        let game1Victor = document.querySelector(`[game_block_${i}] select[game_1_winner]`)
+        var bet = {
+            "userId": "",
+            "roundId": 1,
+            "games": []
+        };
+        for (i = 0; i < 6; i++) {
+            let teamATries = document.querySelector(`[game_block_${i}] input[team_A_tries]`)
+            let teamBTries = document.querySelector(`[game_block_${i}] input[team_B_tries]`)
+            let game1Victor = document.querySelector(`[game_block_${i}] select[game_1_winner]`)
+            bet.games.push({
+                teamATries: teamATries.value,
+                teamBTries: teamBTries.value,
+                game1Victor: game1Victor.value
+            });
+        };
+        let goldenTryInput = document.querySelector('input[golden_try_input]');
         bet.games.push({
-            teamATries: teamATries.value,
-            teamBTries: teamBTries.value,
-            game1Victor: game1Victor.value
+                goldenTrySelection: goldenTryInput.value
         });
-    }
-    return bet;
-}
+            return bet;
+        };
