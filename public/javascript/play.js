2dc55be02055db79ffe89de18520894d37450abb
Chris <chris171819@googlemail.com>
2019-10-27 15:53:14 +0000

    changed to create bet in bet.js to accept the obj

--- public/javascript/play.js
+++ public/javascript/play.js
@@ -14,11 +14,11 @@ var getBetValues = () => {
         for (i = 0; i < 6; i++) {
             let teamATries = document.querySelector(`[game_block_${i}] input[team_A_tries]`)
             let teamBTries = document.querySelector(`[game_block_${i}] input[team_B_tries]`)
-            let game1Victor = document.querySelector(`[game_block_${i}] select[game_1_winner]`)
+            let gameVictor = document.querySelector(`[game_block_${i}] select[game_1_winner]`)
             bet.games.push({
                 teamATries: teamATries.value,
                 teamBTries: teamBTries.value,
-                game1Victor: game1Victor.value
+                gameVictor: gameVictor.value
             });
         };
         let goldenTryInput = document.querySelector('input[golden_try_input]');
--- services/bets.js
+++ services/bets.js
@@ -1,25 +1,49 @@
-
 const createBet = (app, bet) => {
-    const db = app.get('super6db');
-    const result = db
-        .collection('bets')
-        .insertOne({
-            userId: bet.userId,
-            gameId: bet.gameId,
-            roundId: bet.roundId,
-            teamATries: bet.teamATries,
-            teamBTries: bet.teamBTries,
-            winTeam: bet.winTeam,
-            goldenTry: bet.goldenTry
-        });
-        // bet should contain `games` as an array of 6 items which will have the bets for each game
+    const db = app.get("super6db");
+    const result = db.collection("bets").insertOne({
+        userId: bet.userId,
+        gameId: bet.gameId,
+        roundId: bet.roundId,
+        gameBets: [
+            {
+                teamATries: bet.games[0].teamATries,
+                teamBTries: bet.games[0].teamBTries,
+                winTeam: bet.games[0].gameVictor
+            },
+            {
+                teamATries: bet.games[1].teamATries,
+                teamBTries: bet.games[1].teamBTries,
+                winTeam: bet.games[1].gameVictor
+            },
+            {
+                teamATries: bet.games[2].teamATries,
+                teamBTries: bet.games[2].teamBTries,
+                winTeam: bet.games[2].gameVictor
+            },
+            {
+                teamATries: bet.games[3].teamATries,
+                teamBTries: bet.games[3].teamBTries,
+                winTeam: bet.games[3].gameVictor
+            },
+            {
+                teamATries: bet.games[4].teamATries,
+                teamBTries: bet.games[4].teamBTries,
+                winTeam: bet.games[4].gameVictor
+            },
+            {
+                teamATries: bet.games[5].teamATries,
+                teamBTries: bet.games[5].teamBTries,
+                winTeam: bet.games[5].gameVictor
+            }
+        ],
+        goldenTry: bet.games[6].goldenTrySelection
+    });
+    // bet should contain `games` as an array of 6 items which will have the bets for each game
     return result;
 };
 
-const DeleteBet = (app, betId) => {
-
-}
+const DeleteBet = (app, betId) => { };
 
 module.exports = {
     createBet
-}
+};
