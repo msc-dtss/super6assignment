573f23f3028422dd1595d291b4ffa73895f9c3a6
Tiago Correia <tiago.f.a.correia@gmail.com>
2019-10-27 19:21:32 +0000

    #17 - Added more bet fetching methods.

--- services/bets.js
+++ services/bets.js
@@ -1,5 +1,8 @@
 /**
- * todo
+ * Creates a bet for a user
+ * @param {*} app The express app
+ * @param {Number} roundId The ID of the round
+ * @return {Promise} A promise with the result?
  */
 const createBet = (app, bet) => {
     const db = app.get('super6db');
@@ -28,6 +31,63 @@ const deleteBet = (app, betID, userID) => {
     // do the thing
 }
 
+/**
+ * Gets the bet made by a user
+ * @param {*} app The express app
+ * @param {*} criteria An object with the criteria to find bets
+ * @return {Promise} A promise with an array of bets
+ */
+const fetch = (app, criteria) => {
+    const db = app.get("super6db");
+    return db.collection("bets")
+        .find(criteria)
+        .toArray();
+};
+
+/**
+ * Gets the bet made by a user
+ * @param {*} app The express app
+ * @param {Number} roundId The ID of the round
+ * @return {Promise} A promise with an array of bets
+ */
+const madeByUser = (app, roundId, userId) => {
+    return fetch(app, {
+        roundId,
+        userId
+    });
+};
+
+/**
+ * Gets a collection of bets made by users for a given round
+ * @param {*} app The express app
+ * @param {Number} roundId The ID of the round
+ * @return {Promise} A promise with an array of bets
+ */
+const forRound = (app, roundId) => {
+    return fetch(app, {
+        roundId
+    });
+};
+
+/**
+ * Gets the winning bets by building the result into the criteria of the query
+ * @param {*} app The express app
+ * @param {Number} roundId The ID of the round
+ * @param {*} results The actual game results. These should be in the same format as `bet.gameBets`
+ * @return {Promise} A promise with an array of bets
+ */
+const findRoundWinners = (app, roundId, results) => {
+    return fetch({
+        roundId,
+        gameBets: results.games
+    });
+};
+
+
 module.exports = {
-    createBet
-}
+    createBet,
+    fetch,
+    madeByUser,
+    forRound,
+    findRoundWinners
+};
