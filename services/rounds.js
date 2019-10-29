57c11dc3ffc693581532cd9d13e30a6a5b8ef404
Dosaki <lord.g.bug@gmail.com>
2019-10-29 21:12:09 +0000

    Fixed using app rather than db.

--- services/rounds.js
+++ services/rounds.js
@@ -1,16 +1,15 @@
 const gameService = require('../services/game');
 
-const fetch = (app, criteria) => {
+const fetch = (db, criteria) => {
     // TODO - Order by round
-    const db = app.get('super6db');
     return db
         .collection('rounds')
         .find(criteria)
         .toArray();
 };
 
-const fetchRound = (app, round) => {
-    return gameService.fetch(app, {round_id: round});
+const fetchRound = (db, round) => {
+    return gameService.fetch(db, { round_id: round });
 };
 
 module.exports = {
