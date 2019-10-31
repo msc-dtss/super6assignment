3c0dc159231acd6518c12c09fbb243b0a7b0fb90
Chris <chris171819@googlemail.com>
2019-10-31 20:51:23 +0000

    Seperated onreadystatechange

--- public/javascript/play.js
+++ public/javascript/play.js
@@ -1,19 +1,19 @@
 var makeRequest = (content) => {
     var httpRequest = new XMLHttpRequest();
-
     if (!httpRequest) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
-    };
+    } else {
     httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === 4 && httpRequest.status === 200) {
             console.log(httpRequest.responseText);
+            }
         }
         httpRequest.open('POST', '/bets');
         httpRequest.setRequestHeader('Content-Type', 'application/json');
         httpRequest.send(JSON.stringify(content));
     }
-
+};
     var placeBet = (roundId, nrGames) => {
         var bet = getBetValues(roundId, nrGames);
         console.log(bet)
