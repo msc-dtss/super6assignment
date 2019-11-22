3c2b03520f2c747e45ba5de652140084de9e718b
Tiago Correia <tiago.f.a.correia@gmail.com>
2019-11-22 00:40:10 +0000

    Changed how you get the golden tries to be more inline with how you'd get the data from an API

--- services/results.js
+++ services/results.js
@@ -23,117 +23,119 @@ const refreshResults = async (db) => {
  */
 const getResultsFromApi = async () => {
     return new Promise((resolve, reject) => {
-        //Any transformation that we would need should've happen here.
-        resolve(
-            [
-                {
-                    roundIndex: 0,
-                    games: [
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0f8b',
-                            winTeam: 'Japan',
-                            teamAScore: 30,
-                            teamBScore: 10,
-                            teamATries: 4,
-                            teamBTries: 1
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0f96',
-                            winTeam: 'Australia',
-                            teamAScore: 39,
-                            teamBScore: 21,
-                            teamATries: 6,
-                            teamBTries: 2
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0fa1',
-                            winTeam: 'France',
-                            teamAScore: 23,
-                            teamBScore: 21,
-                            teamATries: 2,
-                            teamBTries: 2
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0fac',
-                            winTeam: 'NewZealand',
-                            teamAScore: 23,
-                            teamBScore: 13,
-                            teamATries: 2,
-                            teamBTries: 1
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0f8d',
-                            winTeam: 'Italy',
-                            teamAScore: 48,
-                            teamBScore: 7,
-                            teamATries: 7,
-                            teamBTries: 1
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0fae',
-                            winTeam: 'Italy',
-                            teamAScore: 47,
-                            teamBScore: 22,
-                            teamATries: 7,
-                            teamBTries: 3
-                        }
-                    ]
-                },
-                {
-                    roundIndex: 1,
-                    games:[
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0faf',
-                            winTeam: 'Ireland',
-                            teamAScore: 27,
-                            teamBScore: 3,
-                            teamATries: 4,
-                            teamBTries: 0
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0fb0',
-                            winTeam: 'England',
-                            teamAScore: 35,
-                            teamBScore: 3,
-                            teamATries: 4,
-                            teamBTries: 0
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0fb1',
-                            winTeam: 'Wales',
-                            teamAScore: 43,
-                            teamBScore: 14,
-                            teamATries: 6,
-                            teamBTries: 2
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0fb2',
-                            winTeam: 'Samoa',
-                            teamAScore: 9,
-                            teamBScore: 34,
-                            teamATries: 0,
-                            teamBTries: 6
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0f8c',
-                            winTeam: 'Uruguay',
-                            teamAScore: 27,
-                            teamBScore: 30,
-                            teamATries: 5,
-                            teamBTries: 3
-                        },
-                        {
-                            gameId: '5dbb5ba824a8c8398c3a0f8e',
-                            winTeam: 'England',
-                            teamAScore: 45,
-                            teamBScore: 7,
-                            teamATries: 7,
-                            teamBTries: 1
-                        }
-                    ]
-                }
-            ]
-        );
+        //This would be what we'd get from the API.
+        const apiResult = [
+            {
+                roundIndex: 0,
+                games: [
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0f8b',
+                        winTeam: 'Japan',
+                        teamAScore: 30,
+                        teamBScore: 10,
+                        teamATries: 4,
+                        teamBTries: 1
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0f96',
+                        winTeam: 'Australia',
+                        teamAScore: 39,
+                        teamBScore: 21,
+                        teamATries: 6,
+                        teamBTries: 2
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0fa1',
+                        winTeam: 'France',
+                        teamAScore: 23,
+                        teamBScore: 21,
+                        teamATries: 2,
+                        teamBTries: 2
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0fac',
+                        winTeam: 'NewZealand',
+                        teamAScore: 23,
+                        teamBScore: 13,
+                        teamATries: 2,
+                        teamBTries: 1
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0f8d',
+                        winTeam: 'Italy',
+                        teamAScore: 48,
+                        teamBScore: 7,
+                        teamATries: 7,
+                        teamBTries: 1
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0fae',
+                        winTeam: 'Italy',
+                        teamAScore: 47,
+                        teamBScore: 22,
+                        teamATries: 7,
+                        teamBTries: 3
+                    }
+                ],
+                goldenTry: 5
+            },
+            {
+                roundIndex: 1,
+                games: [
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0faf',
+                        winTeam: 'Ireland',
+                        teamAScore: 27,
+                        teamBScore: 3,
+                        teamATries: 4,
+                        teamBTries: 0
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0fb0',
+                        winTeam: 'England',
+                        teamAScore: 35,
+                        teamBScore: 3,
+                        teamATries: 4,
+                        teamBTries: 0
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0fb1',
+                        winTeam: 'Wales',
+                        teamAScore: 43,
+                        teamBScore: 14,
+                        teamATries: 6,
+                        teamBTries: 2
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0fb2',
+                        winTeam: 'Samoa',
+                        teamAScore: 9,
+                        teamBScore: 34,
+                        teamATries: 0,
+                        teamBTries: 6
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0f8c',
+                        winTeam: 'Uruguay',
+                        teamAScore: 27,
+                        teamBScore: 30,
+                        teamATries: 5,
+                        teamBTries: 3
+                    },
+                    {
+                        gameId: '5dbb5ba824a8c8398c3a0f8e',
+                        winTeam: 'England',
+                        teamAScore: 45,
+                        teamBScore: 7,
+                        teamATries: 7,
+                        teamBTries: 1
+                    }
+                ],
+                goldenTry: 5
+            }
+        ];
+        //Any transformation that we would need should happen here.
+        resolve(apiResult);
     });
 }
 
@@ -144,7 +146,7 @@ const getResultsFromApi = async () => {
  * @returns {Array} The array of results
  */
 const getResults = async (isForced) => {
-    if(isForced){
+    if (isForced) {
         resultsCache = await getResultsFromApi();
     }
     return resultsCache;
@@ -156,36 +158,35 @@ const getResults = async (isForced) => {
  * @returns {*} A json object of each game indexed by the game Id
  */
 const getGameResults = async (isForced) => {
-    if(isForced){
+    if (isForced) {
         resultsCache = await getResultsFromApi();
     }
 
     const gameResults = resultsCache
-        .reduce((acc, result)=>{
+        .reduce((acc, result) => {
             return [...acc, ...result.games];
         }, [])
         .reduce((acc, game) => {
             acc[game.gameId] = game;
             return acc;
         }, {});
-    
+
     return gameResults;
 };
 
-const getGoldenTryResults = async () => {
-    return new Promise((resolve, reject) => {
-        resolve(
-            {
-                '5dc9d60cc46c6d373ce167fd': {
-                    goldenTry: 5
-                },
-                '5dc9d71ba48afe373caee7e3': {
-                    goldenTry: 5
-                }
 
-            }
-        );
-    });
+/**
+ * Returns the goldenTry results 
+ * @returns {*} A json object of round indexed by it's roundIndex
+ */
+const getGoldenTryResults = async (isForced) => {
+    if (isForced) {
+        resultsCache = await getResultsFromApi();
+    }
+    return resultsCache.reduce((acc, result) => {
+        acc[result.roundIndex] = result.goldenTry;
+        return acc;
+    }, {});
 };
 
 module.exports = {
