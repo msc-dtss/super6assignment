fbd661e002105bdca41455d9950e06c1c1f09d39
Tiago Correia <tiago.f.a.correia@gmail.com>
2019-11-22 12:36:31 +0000

    Removed todo.

--- routes/helpers/exceptionHandler.js
+++ routes/helpers/exceptionHandler.js
@@ -1,7 +1,6 @@
 
 /**
  * This method wraps async routes so that we can catch their exceptions and forward them to the appropriate exception handler.
- * TODO: need to find a way to automatically wrap any route endpoint.
  * @param {express.Router} route 
  */
 const exceptionWrapper = (route) => {
