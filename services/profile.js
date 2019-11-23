68cde57162d3f4113706680988d8b79cbc2d89b5
Chris <chris171819@googlemail.com>
2019-11-23 16:22:17 +0000

    Profile page WIP

--- services/profile.js
+++ services/profile.js
@@ -1,7 +1,7 @@
 const userService = require('../services/users');
 const betService = require('../services/bets');
-const roundService = require ('../services/rounds')
-const gameService = require ('../services/game')
+const roundService = require('../services/rounds')
+const gameService = require('../services/game')
 
 /**
  * Grabs the user profile page. This includes the user information as well as the bets history.
@@ -12,20 +12,20 @@ const gameService = require ('../services/game')
 const fetchProfileBundle = async (db, userId) => {
     const users = await userService.fetchById(db, userId); // TODO do we need this becuase of session info?
     const bets = await betService.fetchByUser(db, userId);
-    
+
     // Get the most recent round Index of bets placed
     const recentRoundIndex = () => {
         var recentRound = 0
         for (let i = 0; i < bets.length; i++) {
-            if (recentRound < bets[i].roundIndex){
+            if (recentRound < bets[i].roundIndex) {
                 recentRound = bets[i].roundIndex;
             };
         };
         return recentRound;
     };
     const mostRecentRound = recentRoundIndex();
-    
-    const recentBet = await betService.fetch(db, {userId: userId, roundIndex: mostRecentRound});
+
+    const recentBet = await betService.fetch(db, { userId: userId, roundIndex: mostRecentRound });
 
     // Gets the total points from all placed bets
     let totalPoints = 0;
@@ -36,8 +36,8 @@ const fetchProfileBundle = async (db, userId) => {
     // Gets the number of times the user has bet on rounds previously
     const totalBets = bets.length;
 
-    const rounds = await roundService.fetch(db, {index: mostRecentRound});
-    const games = await gameService.fetch(db, {roundIndex: mostRecentRound});
+    const rounds = await roundService.fetch(db, { index: mostRecentRound });
+    const games = await gameService.fetch(db, { roundIndex: mostRecentRound });
 
     // Grabs todays date & formats to "yyyy/mm/dd"
     const todaysDateGetter = () => {
@@ -53,7 +53,7 @@ const fetchProfileBundle = async (db, userId) => {
     return {
         user: users[0],
         bets: recentBet,
-        round: rounds,  
+        round: rounds,
         games: games,
         totalPoints: totalPoints,
         totalBets: totalBets,
