3a6ae47919c619916a4ae48c715672e6ab01479e
Christopher Smith <chris171819@googlemail.com>
2019-11-24 11:51:54 +0000

    Profile page WIP added alert box to edit bet button

--- public/javascript/profilePage.js
+++ public/javascript/profilePage.js
@@ -1,19 +1,13 @@
-function editTheBets(e) {
-
+function displayEditBetAlert(e) {
     document.querySelector('.edit_bet_button').style.display = 'none';
-    document.querySelector('.save_bet_button').style.display = 'block';
-
-    let fullList = document.querySelectorAll(".bets_text");
-    if (fullList) {
-        for (let i = 0; i < fullList.length; i++) {
-            fullList[i].style.display = "none";
-            let betList = document.querySelectorAll(".bets_edit");
-            betList[i].style.display = "block";
-        };
-    };
+    document.querySelector('.edit_bet_alert').style.display = 'block';
 };
 
-function saveTheBets (e) {
+function yesAlertSelect(e) {
+    console.log(document.response) //Why is response not defined here?
+};
+
+function noAlertSelect(e) {
     document.querySelector('.edit_bet_button').style.display = 'block';
-    document.querySelector('.save_bet_button').style.display = 'none';
+    document.querySelector('.edit_bet_alert').style.display = 'none';
 };
